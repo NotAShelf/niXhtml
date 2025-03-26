baef0a1f6bd449818d42bea14f92a941676078aa
NotAShelf <raf@notashelf.dev>
2025-03-26 14:26:14 +0300

    pkgs -> examples

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       # TODO: more examples, ideally multi-page cases and examples
       # using nixpkgs library (or packages) to write pages with a given
       # structure.
-      singlepage = import ./pkgs/single-page.nix {inherit (self.lib) makePage;};
+      singlepage = import ./examples/single-page.nix {inherit (self.lib) makePage;};
     };
   };
 }
