5d0fcc4544b38b4d30812dba48333f1ed2473322
NotAShelf <raf@notashelf.dev>
2025-03-27 19:12:02 +0300

    use a list to preserve element ordering

--- examples/single-page.nix
+++ examples/single-page.nix
@@ -19,104 +19,112 @@ builtins.toFile "index.html" (makePage {
       div = {
         "@class" = "container";
 
-        header = {
-          "@class" = "site-header";
-          h1 = {
-            "@class" = "site-title";
-            _text = "Welcome to the Nix Site";
-          };
-          nav = {
-            "@class" = "main-nav";
-            ul = [
-              {
-                li = {
-                  a = {
-                    "@href" = "index.xhtml";
-                    "@class" = "active";
-                    _text = "Home";
-                  };
-                };
-              }
-              {
-                li = {
-                  a = {
-                    "@href" = "about.xhtml";
-                    _text = "About";
-                  };
-                };
-              }
-              {
-                li = {
-                  a = {
-                    "@href" = "contact.xhtml";
-                    _text = "Contact";
-                  };
-                };
-              }
-            ];
-          };
-        };
-
-        main = {
-          "@class" = "site-content";
-          _fragment = [
-            {
-              div = {
-                "@class" = "intro-section";
-                p = ''
-                  This page was generated purely in Nix. I didn't know that was possible, but now <em>you</em> do.
-
-                  Honestly, you might even be able to <i>inline some HTML here</i>
-                '';
+        _fragment = [
+          {
+            header = {
+              "@class" = "site-header";
+              h1 = {
+                "@class" = "site-title";
+                _text = "Welcome to the Nix Site";
               };
-            }
-            {_comment = "This is an HTML comment. You should see this in the site source";}
-            {
-              section = {
-                "@class" = "features-section";
-                h2 = "Features";
+              nav = {
+                "@class" = "main-nav";
                 ul = [
                   {
                     li = {
-                      "@class" = "feature-item";
-                      _text = "Fast (lie)";
+                      a = {
+                        "@href" = "index.xhtml";
+                        "@class" = "active";
+                        _text = "Home";
+                      };
                     };
                   }
                   {
                     li = {
-                      "@class" = "feature-item";
-                      _text = "Reproducible";
+                      a = {
+                        "@href" = "about.xhtml";
+                        _text = "About";
+                      };
                     };
                   }
                   {
                     li = {
-                      "@class" = "feature-item";
-                      _text = "Minimalist";
+                      a = {
+                        "@href" = "contact.xhtml";
+                        _text = "Contact";
+                      };
                     };
                   }
                 ];
               };
-            }
-            {
-              div = {
-                "@class" = "logo-container";
-                img = {
-                  "@src" = "nix-logo.png";
-                  "@alt" = "Nix Logo";
-                  "@width" = "200";
-                  "@height" = "100";
-                };
-              };
-            }
-          ];
-        };
+            };
+          }
 
-        footer = {
-          "@class" = "site-footer";
-          p = {
-            _text = "© 2025 nsg";
-          };
-        };
+          {
+            main = {
+              "@class" = "site-content";
+              _fragment = [
+                {
+                  div = {
+                    "@class" = "intro-section";
+                    p = ''
+                      This page was generated purely in Nix. I didn't know that was possible, but now <em>you</em> do.
+
+                      Honestly, you might even be able to <i>inline some HTML here</i>
+                    '';
+                  };
+                }
+                {_comment = "This is an HTML comment. You should see this in the site source";}
+                {
+                  section = {
+                    "@class" = "features-section";
+                    h2 = "Features";
+                    ul = [
+                      {
+                        li = {
+                          "@class" = "feature-item";
+                          _text = "Fast (lie)";
+                        };
+                      }
+                      {
+                        li = {
+                          "@class" = "feature-item";
+                          _text = "Reproducible";
+                        };
+                      }
+                      {
+                        li = {
+                          "@class" = "feature-item";
+                          _text = "Minimalist";
+                        };
+                      }
+                    ];
+                  };
+                }
+                {
+                  div = {
+                    "@class" = "logo-container";
+                    img = {
+                      "@src" = "nix-logo.png";
+                      "@alt" = "Nix Logo";
+                      "@width" = "200";
+                      "@height" = "100";
+                    };
+                  };
+                }
+              ];
+            };
+          }
+
+          {
+            footer = {
+              "@class" = "site-footer";
+              p = {
+                _text = "© 2025 nsg";
+              };
+            };
+          }
+        ];
       };
     };
   };
